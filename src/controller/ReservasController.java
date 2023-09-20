249f550be9b183d126d2ddb2e3990178bfffb551
Will <wvmwill@gmail.com>
2023-09-20 07:58:05 -0300

    alteracao do ReservaController com conexao da ReservaDAO

--- src/controller/ReservasController.java
+++ src/controller/ReservasController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import DAO.ReservaDAO;
+import MODELO.Reserva;
+import jdb.factory.ConnectionFactory;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class ReservasController {
+
+    private ReservaDAO reservaDAO;
+
+    public ReservasController(){
+        Connection connection = new ConnectionFactory().recuperarConexao();
+        this.reservaDAO = new ReservaDAO(connection);
+    }
+
+    public void salvar(Reserva reserva){ this.reservaDAO.salvar(reserva); }
+
+    //public List<Reserva> buscar(){ return.this.reservaDAO.buscar;}
 }
+
